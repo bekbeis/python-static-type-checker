ba09089280ab559213e8b6c8062701ea7507f4a3
Bekzat Skakov <bepc@bemac.local>
2023-01-15 19:13:20 +0600

    config changes

--- out/extension.js
+++ out/extension.js
@@ -10,50 +10,22 @@ const child_process_1 = require("child_process");
 function activate(context) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "helloworld" is now active!');
+    console.log('Congratulations, your extension "Python Type Checker" is now active!');
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable1 = vscode.commands.registerCommand('helloworld.helloWorld', () => {
+    let greetDisposable = vscode.commands.registerCommand('pstc.greeting', () => {
         // The code you place here will be executed every time your command is executed
         // Display a message box to the user
         vscode.window.showInformationMessage('Hello VS Code!');
     });
-    let disposable2 = vscode.commands.registerCommand('helloworld.time', () => {
-        let date = new Date();
-        vscode.window.showInformationMessage(date.toString());
-    });
-    let disposable3 = vscode.commands.registerCommand('helloworld.warn', () => {
-        let msg = 'CUSTOM WARNING!';
-        vscode.window.showWarningMessage(msg);
-    });
-    let disposable4 = vscode.commands.registerCommand('helloworld.err', () => {
-        let errMsg = 'OH NO! YOU EVOKED AN ERROR!';
-        vscode.window.showErrorMessage(errMsg);
-    });
-    let disposable5 = vscode.commands.registerCommand('helloworld.java', () => {
+    let javaCallDisposable = vscode.commands.registerCommand('pstc.javaCall', () => {
         (0, child_process_1.execSync)('javac ' + __dirname + '/java/Main.java', { encoding: 'utf-8' });
         const output = (0, child_process_1.execSync)('java -cp ' + __dirname + '/java/ Main', { encoding: 'utf-8' });
         vscode.window.showInformationMessage(output);
     });
-    let disposable6 = vscode.commands.registerCommand('helloworld.getFile', () => {
-        if (vscode.workspace.workspaceFolders !== undefined) {
-            let wf = vscode.workspace.workspaceFolders[0].uri.path;
-            let f = vscode.workspace.workspaceFolders[0].uri.fsPath;
-            let message = `YOUR-EXTENSION: folder: ${wf} - ${f}`;
-            vscode.window.showInformationMessage(message);
-        }
-        else {
-            let message = "YOUR-EXTENSION: Working folder not found, open a folder an try again";
-            vscode.window.showErrorMessage(message);
-        }
-    });
-    context.subscriptions.push(disposable1);
-    context.subscriptions.push(disposable2);
-    context.subscriptions.push(disposable3);
-    context.subscriptions.push(disposable4);
-    context.subscriptions.push(disposable5);
-    context.subscriptions.push(disposable6);
+    context.subscriptions.push(greetDisposable);
+    context.subscriptions.push(javaCallDisposable);
 }
 exports.activate = activate;
 // This method is called when your extension is deactivated
