2ec8f28b93ab6cd0bed6120bdd28762ad0ba55f7
Bekzat Skakov <bepc@bemac.local>
2023-02-02 11:04:16 +0600

    document scanning added

--- out/extension.js
+++ out/extension.js
@@ -1,39 +1,47 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deactivate = exports.activate = void 0;
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
 const child_process_1 = require("child_process");
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "Python Type Checker" is now active!');
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    let greetDisposable = vscode.commands.registerCommand('pstc.greeting', () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello VS Code!');
-    });
-    let javaCallDisposable = vscode.commands.registerCommand('pstc.javaCall', () => {
+    // The code below is a sample of how Java program can be called from the extension environment
+    // IMPORTANT NOTE: There is no better option to implement this functionality for now
+    const javaCallDisposable = vscode.commands.registerCommand('pstc.javaCall', () => {
         (0, child_process_1.execSync)('javac ' + __dirname + '/java/Main.java', { encoding: 'utf-8' });
         const output = (0, child_process_1.execSync)('java -cp ' + __dirname + '/java/ Main', { encoding: 'utf-8' });
         vscode.window.showInformationMessage(output);
     });
-    let getFilePathDisposable = vscode.commands.registerCommand('pstc.getFilePath', () => {
+    // This method is one option for how the contents of the file currently open in the workspace can be accessed
+    // The access is implemented via the file path
+    const getFilePathDisposable = vscode.commands.registerCommand('pstc.getFilePath', () => {
         if (vscode.window.activeTextEditor !== undefined) {
             const currentOpenFilePath = vscode.window.activeTextEditor.document.uri.fsPath;
+            // The code below is to demonstrate whether the file content is accessed successfully
             const catOutput = (0, child_process_1.execSync)('cat ' + currentOpenFilePath, { encoding: 'utf-8' });
             vscode.window.showInformationMessage(catOutput);
         }
     });
-    context.subscriptions.push(greetDisposable);
+    // This method is another option for how the contents of the file currently open in the workspace can be accessed
+    // The access is implemented via the active text editor
+    const scanDocumentDisposable = vscode.commands.registerCommand('pstc.scanDocument', () => {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            let document = editor.document;
+            const documentText = document.getText();
+            // The code below is to demonstrate whether the file content is accessed successfully
+            vscode.window.showInformationMessage(documentText);
+            // IMPORTANT NOTE: Is it possible to compare the instances of documentText?
+            //				   If the answer is yes, we can only send the updated parts
+            //				   of the document to the back end of the system
+        }
+    });
     context.subscriptions.push(javaCallDisposable);
     context.subscriptions.push(getFilePathDisposable);
+    context.subscriptions.push(scanDocumentDisposable);
 }
 exports.activate = activate;
 // This method is called when your extension is deactivated
--- src/extension.ts
+++ src/extension.ts
@@ -1,43 +1,55 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { execSync } from 'child_process';
-import { measureMemory } from 'vm';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "Python Type Checker" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let greetDisposable = vscode.commands.registerCommand('pstc.greeting', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello VS Code!');
-	});
-
-	let javaCallDisposable = vscode.commands.registerCommand('pstc.javaCall', () => {
+	// The code below is a sample of how Java program can be called from the extension environment
+	// IMPORTANT NOTE: There is no better option to implement this functionality for now
+	const javaCallDisposable = vscode.commands.registerCommand('pstc.javaCall', () => {
 		execSync('javac ' + __dirname + '/java/Main.java', { encoding: 'utf-8' });
 		const output = execSync('java -cp ' + __dirname + '/java/ Main', { encoding: 'utf-8' });
 		vscode.window.showInformationMessage(output);
 	});
 
-	let getFilePathDisposable = vscode.commands.registerCommand('pstc.getFilePath', () => {
+	// This method is one option for how the contents of the file currently open in the workspace can be accessed
+	// The access is implemented via the file path
+	const getFilePathDisposable = vscode.commands.registerCommand('pstc.getFilePath', () => {
 		if (vscode.window.activeTextEditor !== undefined) {
 			const currentOpenFilePath = vscode.window.activeTextEditor.document.uri.fsPath;
+			
+			// The code below is to demonstrate whether the file content is accessed successfully
 			const catOutput = execSync('cat ' + currentOpenFilePath, { encoding: 'utf-8'});
 			vscode.window.showInformationMessage(catOutput);
 		}
 	});
 
-	context.subscriptions.push(greetDisposable);
+	// This method is another option for how the contents of the file currently open in the workspace can be accessed
+	// The access is implemented via the active text editor
+	const scanDocumentDisposable = vscode.commands.registerCommand('pstc.scanDocument', () => {
+		const editor = vscode.window.activeTextEditor;
+
+		if (editor) {
+			let document = editor.document;
+			const documentText = document.getText();
+
+			// The code below is to demonstrate whether the file content is accessed successfully
+			vscode.window.showInformationMessage(documentText);
+
+			// IMPORTANT NOTE: Is it possible to compare the instances of documentText?
+			//				   If the answer is yes, we can only send the updated parts
+			//				   of the document to the back end of the system
+		}
+	});
+
+
 	context.subscriptions.push(javaCallDisposable);
 	context.subscriptions.push(getFilePathDisposable);
+	context.subscriptions.push(scanDocumentDisposable);
 }
 
 // This method is called when your extension is deactivated
